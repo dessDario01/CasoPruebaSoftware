9555cce49cb9926dbe337a8e9d742c42078b0f47
laboratorioesan <laboratorioesan@esan.edu.pe>
2026-04-06 21:04:36 -0500

    Actualiza dPregunta3.py

--- dPregunta3.py
+++ dPregunta3.py
@@ -33,3 +33,42 @@ resultado = quicksort(mi_lista)
 
 print("Lista original:", mi_lista)
 print("Lista ordenada:", resultado)
+
+
+
+print("--- PRUEBA DE COBERTURA DE SENTENCIAS ---")
+
+
+# Caso: Una lista desordenada normal
+# Esto obliga al código a calcular el pivote, entrar al for,
+# repartir en las 3 listas y hacer la recursión.
+entrada = [10, 5, 20]
+resultado = quicksort(entrada)
+
+
+if resultado == [5, 10, 20]:
+    print("Prueba PASADA: Se ejecutaron todas las líneas de la función.")
+else:
+    print("Prueba FALLIDA.")
+
+
+print("--- PRUEBA DE COBERTURA DE DECISIONES ---")
+
+
+# 1. Probar la decisión: if len(arr) <= 1
+# Camino VERDADERO (Lista vacía)
+if quicksort([]) == []:
+    print("Decisión 'Lista <= 1' (TRUE): PASADA")
+
+
+# Camino FALSO (Lista con varios elementos)
+if quicksort([5, 2]) == [2, 5]:
+    print("Decisión 'Lista <= 1' (FALSE): PASADA")
+
+
+# 2. Probar las decisiones del bucle FOR (menor, igual, mayor)
+# Usamos una lista donde el pivote sea el del medio (5)
+# El 3 es menor, el 5 es igual, el 8 es mayor.
+entrada_completa = [5, 3, 8]
+if quicksort(entrada_completa) == [3, 5, 8]:
+    print("Decisiones de comparación (<, ==, >): TODAS PASADAS")
